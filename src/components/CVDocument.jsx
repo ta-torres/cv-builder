de99f071e903a39a9c883c773a543d668d532dbc
Thomas <93293510+ta-torres@users.noreply.github.com>
2025-01-14 21:31:19 -0300

    Style page and document preview

--- src/components/CVDocument.jsx
+++ src/components/CVDocument.jsx
@@ -15,7 +15,7 @@ import ArialBoldItalic from "../assets/fonts/arial/arial-bold-italic.ttf";
 const CVDocument = ({ data }) => (
   <Document>
     <Page size="A4" style={styles.cv_preview}>
-      {/* header */}
+      {/* Header */}
       <View style={styles.cv_header}>
         <Text style={styles.name}>{data.generalInfo.data.fullName}</Text>
         <View style={styles.contact_info}>
@@ -25,134 +25,153 @@ const CVDocument = ({ data }) => (
         </View>
       </View>
 
-      <View style={styles.cv_section}>
-        <Text style={styles.cv_section_title}>Summary</Text>
-        <View style={styles.summary_section}>
-          <Text style={styles.summary_text}>{data.summary.data.summary}</Text>
+      <View style={styles.cv_body}>
+        {/* Summary */}
+        <View style={styles.cv_section}>
+          <Text style={styles.cv_section_title}>Summary</Text>
+          <View style={styles.summary_section}>
+            <Text style={styles.summary_text}>{data.summary.data.summary}</Text>
+          </View>
         </View>
-      </View>
 
-      <View style={styles.cv_section}>
-        <Text style={styles.cv_section_title}>Skills</Text>
-        {data.skills.data.map((category, index) => (
-          <View key={index} style={styles.skills_section}>
-            <Text style={styles.skill_category}>{category.category}</Text>
-            <View style={styles.skills_list}>
-              {category.skills
-                .filter((skill) => skill)
-                .map((skill, idx) => (
-                  <View key={idx} style={styles.list_item}>
-                    <View style={styles.list_bullet}>
-                      <Text> •</Text>
+        {/* Skills */}
+        <View style={styles.cv_section}>
+          <Text style={styles.cv_section_title}>Skills</Text>
+          <View style={styles.skills_section}>
+            {data.skills.data.map((category, index) => (
+              <View key={index} style={styles.skills_item}>
+                <Text style={styles.skill_category}>{category.category}</Text>
+                <View style={styles.skills_list}>
+                  {category.skills
+                    .filter((skill) => skill)
+                    .map((skill, idx) => (
+                      <View key={idx} style={styles.list_item}>
+                        <View style={styles.list_bullet}>
+                          <Text> •</Text>
+                        </View>
+                        <Text style={styles.list_text}>{skill}</Text>
+                      </View>
+                    ))}
+                </View>
+              </View>
+            ))}
+          </View>
+        </View>
+
+        {/* Education */}
+        <View style={styles.cv_section}>
+          <Text style={styles.cv_section_title}>Education</Text>
+          <View style={styles.education_section}>
+            {data.education.data.map((edu, index) => (
+              <View key={index} style={styles.education_item}>
+                <View style={styles.education_header}>
+                  <Text style={styles.degree}>{edu.degree}</Text>
+                  <Text style={styles.dates}>
+                    {edu.endDate} - {edu.startDate}
+                  </Text>
+                </View>
+                <View style={styles.education_details}>
+                  <Text style={styles.school}>{edu.school}</Text>
+                  <Text style={styles.location}>{edu.location}</Text>
+                </View>
+              </View>
+            ))}
+          </View>
+        </View>
+
+        {/* Courses and Certifications */}
+        <View style={styles.cv_section}>
+          <Text style={styles.cv_section_title}>
+            Courses and Certifications
+          </Text>
+          <View style={styles.education_section}>
+            {data.courses.data.map((course, index) => (
+              <View key={index} style={styles.education_item}>
+                <View style={styles.course_header}>
+                  <View style={styles.course_title}>
+                    <Text style={styles.course_name}>{course.name}</Text>
+                    <Text style={styles.course_provider}>
+                      {" "}
+                      - {course.provider}
+                    </Text>
+                  </View>
+                  <Text style={styles.dates}>{course.year}</Text>
+                </View>
+                {course.description && (
+                  <View style={styles.list}>
+                    <View style={styles.list_item}>
+                      <View style={styles.list_bullet}>
+                        <Text>•{"  "}</Text>
+                      </View>
+                      <Text style={styles.list_text}>{course.description}</Text>
                     </View>
-                    <Text style={styles.list_text}>{skill}</Text>
                   </View>
-                ))}
-            </View>
-          </View>
-        ))}
-      </View>
-
-      {/* Education */}
-      <View style={styles.cv_section}>
-        <Text style={styles.cv_section_title}>Education</Text>
-        {data.education.data.map((edu, index) => (
-          <View key={index} style={styles.education_item}>
-            <View style={styles.education_header}>
-              <Text style={styles.degree}>{edu.degree}</Text>
-              <Text style={styles.dates}>
-                {edu.endDate} - {edu.startDate}
-              </Text>
-            </View>
-            <View style={styles.education_details}>
-              <Text style={styles.school}>{edu.school}</Text>
-              <Text style={styles.location}>{edu.location}</Text>
-            </View>
-          </View>
-        ))}
-      </View>
-
-      <View style={styles.cv_section}>
-        <Text style={styles.cv_section_title}>Courses and Certifications</Text>
-        {data.courses.data.map((course, index) => (
-          <View key={index} style={styles.education_item}>
-            <View style={styles.course_header}>
-              <View style={styles.course_title}>
-                <Text style={styles.course_name}>{course.name}</Text>
-                <Text style={styles.course_provider}> - {course.provider}</Text>
+                )}
               </View>
-              <Text style={styles.dates}>{course.year}</Text>
-            </View>
-            {course.description && (
-              <View style={styles.list}>
-                <View style={styles.list_item}>
-                  <View style={styles.list_bullet}>
-                    <Text>•{"  "}</Text>
-                  </View>
-                  <Text style={styles.list_text}>{course.description}</Text>
+            ))}
+          </View>
+        </View>
+
+        {/* Experience */}
+        <View style={styles.cv_section}>
+          <Text style={styles.cv_section_title}>Experience</Text>
+          <View style={styles.experience_section}>
+            {data.experience.data.map((exp, index) => (
+              <View key={index} style={styles.experience_item}>
+                <View style={styles.experience_header}>
+                  <Text style={styles.position}>{exp.position}</Text>
+                  <Text style={styles.dates}>
+                    {exp.endDate} - {exp.startDate}
+                  </Text>
+                </View>
+
+                <View style={styles.experience_details}>
+                  <Text style={styles.company}>{exp.company}</Text>
+                  <Text style={styles.location}>{exp.location}</Text>
+                </View>
+
+                <View style={styles.list}>
+                  {exp.responsibilities.map((responsibility, idx) => (
+                    <View key={idx} style={styles.list_item}>
+                      <View style={styles.list_bullet}>
+                        <Text>•{"  "}</Text>
+                      </View>
+                      <Text style={styles.list_text}>{responsibility}</Text>
+                    </View>
+                  ))}
                 </View>
               </View>
-            )}
+            ))}
           </View>
-        ))}
-      </View>
+        </View>
 
-      {/* Experience */}
-      <View style={styles.cv_section}>
-        <Text style={styles.cv_section_title}>Experience</Text>
-        {data.experience.data.map((exp, index) => (
-          <View key={index} style={styles.experience_item}>
-            <View style={styles.experience_header}>
-              <Text style={styles.position}>{exp.position}</Text>
-              <Text style={styles.dates}>
-                {exp.endDate} - {exp.startDate}
-              </Text>
-            </View>
-
-            <View style={styles.experience_details}>
-              <Text style={styles.company}>{exp.company}</Text>
-              <Text style={styles.location}>{exp.location}</Text>
-            </View>
-
-            <View style={styles.list}>
-              {exp.responsibilities.map((responsibility, idx) => (
-                <View key={idx} style={styles.list_item}>
-                  <View style={styles.list_bullet}>
-                    <Text>•{"  "}</Text>
-                  </View>
-                  <Text style={styles.list_text}>{responsibility}</Text>
+        {/* Personal Projects */}
+        <View style={styles.cv_section}>
+          <Text style={styles.cv_section_title}>Personal Projects</Text>
+          <View style={styles.experience_section}>
+            {data.projects.data.map((project, index) => (
+              <View key={index} style={styles.experience_item}>
+                <View style={styles.experience_header}>
+                  <Text style={styles.position}>{project.name}</Text>
+                  <Text style={styles.company}>{project.link}</Text>
                 </View>
-              ))}
-            </View>
-          </View>
-        ))}
-      </View>
 
-      <View style={styles.cv_section}>
-        <Text style={styles.cv_section_title}>Personal Projects</Text>
-        {data.projects.data.map((project, index) => (
-          <View key={index} style={styles.experience_item}>
-            <View style={styles.experience_header}>
-              <Text style={styles.position}>{project.name}</Text>
-              <Text style={styles.company}>{project.link}</Text>
-            </View>
+                <Text>{project.description}</Text>
 
-            <View style={styles.experience_details}>
-              <Text>{project.description}</Text>
-            </View>
-
-            <View style={styles.list}>
-              {project.features.map((feature, idx) => (
-                <View key={idx} style={styles.list_item}>
-                  <View style={styles.list_bullet}>
-                    <Text>•{"  "}</Text>
-                  </View>
-                  <Text style={styles.list_text}>{feature}</Text>
+                <View style={styles.list}>
+                  {project.features.map((feature, idx) => (
+                    <View key={idx} style={styles.list_item}>
+                      <View style={styles.list_bullet}>
+                        <Text>•{"  "}</Text>
+                      </View>
+                      <Text style={styles.list_text}>{feature}</Text>
+                    </View>
+                  ))}
                 </View>
-              ))}
-            </View>
+              </View>
+            ))}
           </View>
-        ))}
+        </View>
       </View>
     </Page>
   </Document>
@@ -172,18 +191,12 @@ const styles = StyleSheet.create({
   cv_preview: {
     background: "white",
     padding: "40px",
-    // maxWidth: "800px",
-    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
     fontFamily: "Arial",
     fontSize: "12px",
-    display: "flex",
-    flexDirection: "column",
-    gap: "24px",
   },
   cv_header: {
     display: "flex",
     flexDirection: "column",
-    gap: "8px",
     textAlign: "center",
     paddingBottom: "16px",
     borderBottom: "2px solid #333",
@@ -196,26 +209,33 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "row",
     justifyContent: "center",
-    gap: "24px",
+    gap: "12px",
     color: "#666",
     fontSize: "14px",
     alignItems: "baseline",
   },
+  cv_body: {
+    paddingTop: "16px",
+  },
   cv_section: {
     display: "flex",
     flexDirection: "column",
-    gap: "8px",
+    paddingBottom: "9px",
   },
   cv_section_title: {
     color: "#2563eb",
     borderBottom: "1px solid #ddd",
-    paddingBottom: "8px",
     fontWeight: "bold",
     fontSize: "16px",
   },
-  summary_section: {},
+  summary_section: {
+    paddingTop: "4px",
+  },
   summary_text: {},
   skills_section: {
+    paddingTop: "4px",
+  },
+  skills_item: {
     display: "flex",
     flexDirection: "row",
   },
@@ -229,13 +249,17 @@ const styles = StyleSheet.create({
     paddingLeft: "8px",
     lineHeight: "0.8",
   },
-  education_item: {},
+  education_section: {
+    paddingTop: "4px",
+  },
+  education_item: {
+    paddingBottom: "4px",
+  },
   education_header: {
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "baseline",
-    paddingBottom: "2px",
   },
   education_details: {
     display: "flex",
@@ -245,28 +269,28 @@ const styles = StyleSheet.create({
   degree: {
     fontWeight: "bold",
   },
-  school: {
-    fontWeight: "bold",
-    color: "#666",
-  },
+  school: {},
   location: {
     color: "#666",
   },
   dates: {},
-  experience_item: {},
+  experience_section: {
+    paddingTop: "4px",
+  },
+  experience_item: {
+    paddingBottom: "4px",
+  },
   experience_header: {
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "baseline",
-    paddingBottom: "2px",
   },
   experience_details: {
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
     color: "#666",
-    paddingBottom: "8px",
   },
   position: {
     fontWeight: "bold",
@@ -283,7 +307,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
   },
   list_bullet: {
-    transform: "translateY(-2px)",
+    transform: "translateY(-3px)",
     fontSize: "18px",
   },
   list_text: {},
